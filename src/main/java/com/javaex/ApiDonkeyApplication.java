ca7298265ca893052676e254265dea3d782111dc
Yuchan Kim <yuchankim0906@gmail.com>
2024-11-16 15:21:52 +0900

    Initializing the project

---
+++ src/main/java/com/javaex/ApiDonkeyApplication.java
@@ -0,0 +1,13 @@
+package com.javaex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiDonkeyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiDonkeyApplication.class, args);
+	}
+
+}
