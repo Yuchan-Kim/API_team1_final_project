bb071972b94948c263532f87f642c0675f11a9fe
함민규 <antrophus@yahoo.com>
2024-12-06 02:06:12 +0900

    중복 포인트 지금 좀 없애자;;;

--- src/main/java/com/javaex/service/NaverUserService.java
+++ src/main/java/com/javaex/service/NaverUserService.java
@@ -18,7 +18,7 @@ public class NaverUserService {
     /* 네이버 회원 가입 */
     public HmkSocialUserVo SetNaverUser(HmkSocialUserVo userVo) {
         HmkSocialUserVo existingUser = userDao.selectUserByEmail(userVo.getUserEmail());
-        System.out.println("서비스가 받은거: " + userVo);
+        System.out.println("서비스가 받은거: 포인트는 신규만줘 제발!!" + userVo);
         if (existingUser == null) {
             // 새 회원으로 가입
             userVo.setRegionNum(1);
