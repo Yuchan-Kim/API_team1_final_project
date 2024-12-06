5154efdbc697ff833bd0583e57ad990d5574cd8a
함민규 <antrophus@yahoo.com>
2024-12-06 17:41:02 +0900

    WebMvcConfig 코드 추가(서버에서 React앱의 모든 경로 처리 함수)12/6

--- src/main/java/com/javaex/config/WebMvcConfig.java
+++ src/main/java/com/javaex/config/WebMvcConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -54,5 +55,12 @@ public class WebMvcConfig implements WebMvcConfigurer {
 		
         
     }
+	 @Override
+	    public void addViewControllers(ViewControllerRegistry registry) {
+	        registry.addViewController("/{x:[\\w\\-]+}")
+	               .setViewName("forward:/index.html");
+	        registry.addViewController("/**/{x:[\\w\\-]+}")
+	               .setViewName("forward:/index.html");
+	    }
 
 }
